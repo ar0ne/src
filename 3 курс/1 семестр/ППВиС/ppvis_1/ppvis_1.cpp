baa2fb86310611f4ff1dd7f0a4fafcd4a094b276
ar0ne <camtbitakou@yandex.ru>
2014-09-16 21:27:24 +0300

    Добавил первую лабу по ППВиС

---
+++ 3 курс/1 семестр/ППВиС/ppvis_1/ppvis_1.cpp
@@ -0,0 +1,18 @@
+//============================================================================
+// Name        : ppvis_1.cpp
+// Author      : Serj Shalygailo
+// Version     :
+// Copyright   : 
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include "ppvis_1.h"
+#include "truckCarrier.h"
+
+int main() {
+	TruckCarrier tr1, tr2("Pink", "Jaguar", 30, "BB-3333", "BREAK", "Samuel", 14, 14, 56, -4);
+	tr1.showAllInfo();
+	tr2.showAllInfo();
+
+	return 0;
+}
